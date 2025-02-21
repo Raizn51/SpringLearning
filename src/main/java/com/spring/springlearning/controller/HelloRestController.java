956434bb94205e7b3b9619c46ab6eade382dd34e
Raizn51 <Gouravraikwar989@gmail.com>
2025-02-21 19:09:18 +0530

    [Gouarv Raikwar] Add . REST API Hello from BridgeLabz

---
+++ src/main/java/com/spring/springlearning/controller/HelloRestController.java
@@ -0,0 +1,16 @@
+package com.spring.springlearning.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello from BridgeLabz!";
+    }
+}
